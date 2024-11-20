2fdbdfdc836dc4768e6cda97ca4f2ef482dccdbc
Alex Varjabedian <alex.var@simplepractice.com>
2024-11-20 14:45:39 -0800

    add nested routes for comments resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
